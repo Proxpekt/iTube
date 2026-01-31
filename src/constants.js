ef224ce7c98ca56fefcb50f8cd90e503729049e2
Proxpekt <aayushsaxena1008@gmail.com>
2026-01-31 17:24:14 +0530

    Util code added - episode 8

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "iTube";
+export const LIMIT = "16kb";
---
+++ src/utils/ApiError.js
@@ -0,0 +1,28 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message='Something went wrong',
+        errors=[],
+        stack=''
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.data =  null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if(stack){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export default ApiError;
+// ApiError is a custom error class that extends JavaScript’s built-in Error.
+// It allows throwing structured API errors with HTTP status codes, success flag,
+// and optional validation errors. This helps maintain consistent error responses
+// across the application and works with asyncHandler + Express error middleware
+// for centralized error handling.
